b6e1a3f2814de03829ad542c3f2b7ecaeab49cd0
Nur Mohammod <nurm98371@gmail.com>
2025-09-20 12:57:56 +0600

    World Tour

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Welcome the world tour...</h1>
+        </div>
+    );
+};
+
+export default Countries;
